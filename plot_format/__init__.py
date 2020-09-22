1e66ea071a0e85db122be367dae8d312d5f2541e
Okko Järvinen <okko.jarvinen@aalto.fi>
2020-09-22 16:07:32 +0300

    Increased legend text size

--- plot_format/__init__.py
+++ plot_format/__init__.py
@@ -5,6 +5,9 @@ plot_format
 
 Global plot formatting options.
 Goal is to produce IEEE journal compatible figures easily.
+Each plot format setting set in this file can be
+overriden in individual plots, or by setting the respective
+rcParams again.
 
 Initially written by Okko Järvinen, okko.jarvinen@aalto.fi, 2020.
 
@@ -20,11 +23,12 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 from cycler import cycler
+# This cycles first through linestyles with black ('0') color, then 40% gray, etc. 
 monochrome = (cycler('color', ['0.0','0.4','0.6'])*cycler('linestyle', ['-', '--', ':', '-.']))
 
-#plt.rc('axes', prop_cycle=monochrome)
 plt.rcParams['font.weight'] = 'normal'
 plt.rcParams['font.family'] = 'serif'
+plt.rcParams['mathtext.fontset'] = 'dejavuserif'
 plt.rcParams['text.usetex'] = False
 plt.rcParams['axes.labelsize'] = 10
 plt.rcParams['axes.labelweight'] = 'normal'
@@ -35,7 +39,7 @@ plt.rcParams['axes.prop_cycle'] = monochrome
 plt.rcParams['axes.xmargin'] = 0
 plt.rcParams['lines.linewidth'] = 1
 plt.rcParams['lines.markersize'] = 6
-plt.rcParams['legend.fontsize'] = 8
+plt.rcParams['legend.fontsize'] = 10
 plt.rcParams['xtick.labelsize'] = 8
 plt.rcParams['ytick.labelsize'] = 8
 plt.rcParams['figure.titlesize'] = 10
